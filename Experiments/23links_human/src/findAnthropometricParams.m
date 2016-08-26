7b6958c167a450aa54e11ab5ef77c65afb89b427
Claudia Latella <claudia.latella.86@gmail.com>
2016-08-26 10:17:01 +0200

    Fixed Mass and Height concistency.
    Fix https://github.com/claudia-lat/MAPest/issues/1

--- Experiments/23links_human/src/findAnthropometricParams.m
+++ Experiments/23links_human/src/findAnthropometricParams.m
@@ -49,7 +49,7 @@ lowerLegHalfHeight = 0.5 * lowerLegHeight;
 lowerLegRadius     = 0.25 * footGamma; %arbitrarily defined as 1/4 of the foot depth
 lowerLegDiameter   = 0.5 * footGamma;
 % Mass and inertia
-lowerLegMass       = 0.0465 * M;
+lowerLegMass       = 0.0365 * M;
 lowerLegIxx        = (lowerLegMass/12) * (3 * lowerLegRadius^2 + lowerLegHeight^2);
 lowerLegIyy        = (lowerLegMass/12) * (3 * lowerLegRadius^2 + lowerLegHeight^2);
 lowerLegIzz        = (lowerLegMass/2) * (lowerLegRadius^2);
@@ -60,7 +60,7 @@ torsoBeta     = 0.288 * H;
 torsoGamma    = 0.75 * footGamma; %arbitrarily defined as 3/4 of the foot depth
 torsoHalfBeta = 0.5 * torsoBeta;
 % Mass and inertia
-torsoMass     = 0.497 * M;
+torsoMass     = 0.488 * M;
 torsoIxx      = (torsoMass/12) * (torsoAlfa^2 + torsoBeta^2);
 torsoIyy      = (torsoMass/12) * (torsoBeta^2 + torsoGamma^2);
 torsoIzz      = (torsoMass/12) * (torsoGamma^2 + torsoAlfa^2);
@@ -73,7 +73,7 @@ upperLegRadius     = 0.5 * upperLegDiameter;
 HipPosY            = 0.5 * (torsoAlfa-2 * upperLegRadius); % distance between right hip and the mid point of the torso along y
 HipDoublePosY = 2 * HipPosY;
 % Mass and inertia
-upperLegMass       = 0.100 * M;
+upperLegMass       = 0.125 * M;
 upperLegIxx        = (upperLegMass/12) * (3 * upperLegRadius^2 + upperLegHeight^2);
 upperLegIyy        = (upperLegMass/12) * (3 * upperLegRadius^2 + upperLegHeight^2);
 upperLegIzz        = (upperLegMass/2) * (upperLegRadius^2);
@@ -84,7 +84,7 @@ pelvisBeta     = 0.1887 * torsoBeta;
 pelvisGamma    = footGamma; %arbitrarily defined as the foot depth
 pelvisHalfBeta = 0.5 * pelvisBeta;
 % Mass and inertia
-pelvisMass     = 0.1750 * torsoMass;
+pelvisMass     = 0.08 * M;
 pelvisIxx      = (pelvisMass/12) * (pelvisAlfa^2 + pelvisBeta^2);
 pelvisIyy      = (pelvisMass/12) * (pelvisBeta^2 + pelvisGamma^2);
 pelvisIzz      = (pelvisMass/12) * (pelvisGamma^2 + pelvisAlfa^2);
@@ -97,7 +97,7 @@ L5RadiusY    = L5DiameterY/2;
 L5Height     = 0.2004 * torsoBeta;
 L5HalfHeight = 0.5 * L5Height;
 % Mass and inertia
-L5Mass       = 0.0998 * torsoMass;
+L5Mass       = 0.102 * M;
 L5Ixx        = (L5Mass/12) * (3 * L5RadiusY^2 + L5Height^2);
 L5Iyy        = (L5Mass/12) * (3 * L5RadiusX^2 + L5Height^2);
 L5Izz        = (L5Mass/2) * (L5RadiusX * L5RadiusY);
@@ -110,7 +110,7 @@ L3RadiusY    = L3DiameterY/2;
 L3Height     = 0.1810 * torsoBeta;
 L3HalfHeight = 0.5 * L3Height;
 % Mass and inertia
-L3Mass       = 0.0901 * torsoMass;
+L3Mass       = 0.102 * M;
 L3Ixx        = (L3Mass/12) * (3 * L3RadiusY^2 + L3Height^2);
 L3Iyy        = (L3Mass/12) * (3 * L3RadiusX^2 + L3Height^2);
 L3Izz        = (L3Mass/2) * (L3RadiusX * L3RadiusY);
@@ -123,7 +123,7 @@ T12RadiusY    = T12DiameterY/2;
 T12Height     = 0.1810 * torsoBeta;
 T12HalfHeight = 0.5 * T12Height;
 % Mass and inertia
-T12Mass       = 0.0901 * torsoMass;
+T12Mass       = 0.102 * M;
 T12Ixx        = (T12Mass/12) * (3 * T12RadiusY^2 + T12Height^2);
 T12Iyy        = (T12Mass/12) * (3 * T12RadiusX^2 + T12Height^2);
 T12Izz        = (T12Mass/2) * (T12RadiusX * T12RadiusY);
@@ -135,7 +135,7 @@ T8Gamma    = torsoGamma;
 T8HalfBeta = 0.5 * T8Beta;
 T8HalfAlfa = 0.5 * T8Alfa;
 % Mass and inertia
-T8Mass     = 0.0501 * torsoMass;
+T8Mass     = 0.04 * M;
 T8Ixx      = (T8Mass/12) * (T8Alfa^2 + T8Beta^2);
 T8Iyy      = (T8Mass/12) * (T8Beta^2 + T8Gamma^2);
 T8Izz      = (T8Mass/12) * (T8Gamma^2 + T8Alfa^2);
@@ -148,7 +148,7 @@ shoulderHalfHeight = 0.5 * shoulderHeight;
 shoulderPosZ       = T8HalfBeta - shoulderRadius; %distance between the centre of t8 and the joint C7shoulder, along z
 shoulderUrdfPosZ   = T8Beta - shoulderRadius; %distance between the joint jT9T8 and the joint C7shoulder, along z
 % Mass and inertia
-shoulderMass       = 0.2474 * torsoMass;
+shoulderMass       = 0.031 * M;
 shoulderIxx        = (shoulderMass/12) * (3 * shoulderRadius^2 + shoulderHeight^2);
 shoulderIyy        = (shoulderMass/2) * (shoulderRadius^2);
 shoulderIzz        = (shoulderMass/12) * (3 * shoulderRadius^2 + shoulderHeight^2);
@@ -161,7 +161,7 @@ upperArmHalfHeight = 0.5 * upperArmHeight;
 armPosZ            = upperArmHalfHeight - shoulderRadius; %distance between the centre of the upper arm and the joint shoulder, along z
 armUrdfPosZ        = upperArmHalfHeight + armPosZ; %distance between the elbow and the joint shoulder, along z
 % Mass and inertia
-upperArmMass       = 0.028 * M;
+upperArmMass       = 0.030 * M;
 upperArmIxx        = (upperArmMass/12) * (3 * upperArmRadius^2 + upperArmHeight^2);
 upperArmIyy        = (upperArmMass/12) * (3 * upperArmRadius^2 + upperArmHeight^2);
 upperArmIzz        = (upperArmMass/2) * (upperArmRadius^2);
@@ -172,7 +172,7 @@ foreArmDiameter   = foreArmRadius * 2;
 foreArmHeight     = 0.146 * H;
 foreArmHalfHeight = 0.5 * foreArmHeight;
 % Mass and inertia
-foreArmMass       = 0.016 * M;
+foreArmMass       = 0.020 * M;
 foreArmIxx        = (foreArmMass/12) * (3 * foreArmRadius^2 + foreArmHeight^2);
 foreArmIyy        = (foreArmMass/12) * (3 * foreArmRadius^2 + foreArmHeight^2);
 foreArmIzz        = (foreArmMass/2) * (foreArmRadius^2);
@@ -192,21 +192,32 @@ handIzz      = (handMass/12) * (handAlfa^2 + handGamma^2);
 headDiameter = 0.13 * H; %Height of the head
 headRadius   = 0.5 * headDiameter;
 % Mass and inertia
-headMass     = 0.006 * M;
+headMass     = 0.036 * M;
 headIxx      = (2 * headMass/5) * (headRadius^2);
 headIyy      = (2 * headMass/5) * (headRadius^2);
 headIzz      = (2 * headMass/5) * (headRadius^2);
 %% NECK (solid: cylinder)
-% Geometric dimensions
 neckRadius     = 0.5 * headRadius; %arbitrarily defined as the half of the head radius
 neckDiameter   = neckRadius * 2;
-neckHeight     = 0.066 * H;
+neckHeight     = 0.033* H; % neckHeight     = 0.066 * H;
 neckHalfHeight = 0.5 * neckHeight;
 % Mass and inertia
-neckMass       = 0.016 * M;
+neckMass       = 0.012 * M;
 neckIxx        = (neckMass/12) * (3 * neckRadius^2 + neckHeight^2);
 neckIyy        = (neckMass/12) * (3 * neckRadius^2 + neckHeight^2);
 neckIzz        = (neckMass/2) * (neckRadius^2);
+%% Mass and Height consistency
+totalMass = 2*footMass + 2*toeMass + 2*lowerLegMass + 2*upperLegMass ...
+            + pelvisMass + L5Mass + L3Mass  +T12Mass + T8Mass + neckMass + ...
+            headMass + 2*shoulderMass + 2*upperArmMass + 2*foreArmMass + 2*handMass;
+if abs(M - totalMass) > 1e-8
+    error('Somenthing wrong with the mass computation. Check carefully!');
+end
+totalHeight = footBeta + lowerLegHeight + upperLegHeight + torsoBeta +...
+              neckHeight + headDiameter;
+if abs(H - totalHeight) > 1e-8
+        error('Somenthing wrong with the height computation. Check carefully!')
+end
 %% Homogeneous transformations
 pos = [-toeHalfGamma; 0; 0];
 ballFoot_H_toe = [  eye(3)    pos
@@ -260,12 +271,7 @@ pos = [0; 0; foreArmHalfHeight];
 jRightElbow_H_rightForeArm = [ rotX_cc   pos
                               zeros(1,3)  1 ];    
 jLeftElbow_H_leftForeArm = [ rotX_c    pos
-                            zeros(1,3)  1 ];                               
-% pos = [0; 0; foreArmHalfHeight];
-% jRightElbow_H_rightForeArm = [ rotX_cc   pos
-%                               zeros(1,3)  1 ];    
-% jLeftElbow_H_leftForeArm = [ rotX_c    pos
-%                             zeros(1,3)  1 ];
+                            zeros(1,3)  1 ];           
 pos = [0; 0; handHalfBeta];
 jRightWrist_H_rightHand = [ rotX_cc   pos
                            zeros(1,3)  1 ];    
