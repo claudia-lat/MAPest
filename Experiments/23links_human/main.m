922a06ddbd3e013ebc46ec19d8483208048826e2
Claudia Latella <claudia.latella.86@gmail.com>
2016-08-26 19:06:54 +0200

    Added Java path.

--- Experiments/23links_human/main.m
+++ Experiments/23links_human/main.m
@@ -6,6 +6,43 @@ rng(1); % forcing the casual generator to be const
 addpath(genpath('src')); 
 addpath(genpath('../../src'));
 
+%% Java path needed by OSIM
+JAVA_OSIM_PATH = '/usr/local/Cellar/opensim-core/4.0/share/doc/OpenSim/Java';
+JAVA_OSIM_LIB_FOLDER = '/usr/local/Cellar/opensim-core/4.0/lib';
+%get current class path
+javacpath = javaclasspath('-dynamic');
+found = false;
+for i = 1:size(javacpath)
+    if (strcmp(javacpath{i}, JAVA_OSIM_PATH))
+        found = true;
+        break;
+    end
+end
+if ~found
+    javaaddpath(JAVA_OSIM_PATH);
+end
+javapathFile = fullfile(prefdir, 'javalibrarypath.txt');
+found = false;
+if exist(javapathFile, 'file')
+    %open and check file
+    fid = fopen(javapathFile,'r');
+    %for each line, trim and compare with JAVA_OSIM_LIB_FOLDER
+    tline = fgetl(fid);
+    while ischar(tline)
+        if strcmp(strtrim(tline), JAVA_OSIM_LIB_FOLDER)
+            found = true;
+            break;
+        end
+        tline = fgetl(fid);
+    end
+    fclose(fid);
+end
+if ~found
+    fid = fopen(javapathFile,'a');
+    fprintf(fid, strcat(JAVA_OSIM_LIB_FOLDER, '\n'));
+    fclose(fid);
+end
+
 %% Generate subject URDF model
 subjectID = 1;
 H = 1.70; % set manually the heigth in [m] of the subject;
