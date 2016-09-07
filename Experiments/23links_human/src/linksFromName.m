6348e929ed205c2239d389163a5df5d50a63f72a
Claudia Latella <claudia.latella.86@gmail.com>
2016-09-07 10:55:44 +0200

    Moved functions for extracting points and links by name from subjectParamsComputation to src.

---
+++ Experiments/23links_human/src/linksFromName.m
@@ -0,0 +1,22 @@
+function [linkStruct, found] = linksFromName(TotalLinksStruct, linkName)
+%LINKSFROMNAME
+% Inputs:
+% TotalLinkStruct : struct with the amount of links;
+% linkName        : string denoting the link you are looking for;
+% Outputs:
+% linkStruct      : struct related to a link;
+% found           : true if link has been found, false otherwise.
+
+for indx = 1 : size(TotalLinksStruct,1)
+    if  strcmp(TotalLinksStruct{indx,1}.label,linkName)
+        linkStruct = TotalLinksStruct{indx,1};
+        found = true;
+        break;
+    else 
+        found = false;
+    end   
+end
+if found == false
+    error(sprintf('Something wrong in the acquisition! Link label <%s> not found.',linkName));
+end
+end
---
+++ Experiments/23links_human/src/pointsFromName.m
@@ -0,0 +1,22 @@
+function [points, found] = pointsFromName(pointsStruct, pointName)
+%POINTSFROMNAME
+% Inputs:
+% points    : struct of type points;
+% pointName : string denoting the point you are looking for;
+% Outputs:
+% points    : 3x1 vector of points;
+% found     : true if point has been found, false otherwise.
+
+for indx = 1 : pointsStruct.nrOfPoints
+    if  strcmp(pointsStruct.label{1,indx},pointName)
+        points = pointsStruct.pointsValue(:,indx);
+        found = true;
+        break;
+    else 
+        found = false;
+    end   
+end
+if found == false
+    error(sprintf('Something wrong in the acquisition! Point label <%s> not found.',pointName));
+end
+end
