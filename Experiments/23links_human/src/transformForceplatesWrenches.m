5a3e536b8217d818b01fc023b27b2589ca3dcdb8
Claudia Latella <claudia.latella.86@gmail.com>
2017-11-24 18:34:01 +0100

    Modified the transformations for transforming the fp

--- Experiments/23links_human/src/transformForceplatesWrenches.m
+++ Experiments/23links_human/src/transformForceplatesWrenches.m
@@ -26,10 +26,9 @@ function [forceplates] = transformForceplatesWrenches (forceplates, ...
 % forceplate, given by the rawSketch.jpg file. 
 
 %% Preliminary note: 
-% The subject performs the task with the shoes on the plates. The ankle
-% heigths to be considered are a sum of the real ankle height (coming from 
-% subjectParamsFromData) + the fixed height of each shoe = 6.0 cm.
-shoeHeight = 0.060; % in m
+% The subject performs the task with the shoes on the plates.
+% Fixed height of each shoe = 0.060 m
+
 gravityZero = iDynTree.Vector3();
 gravityZero.zero();
 
@@ -48,179 +47,71 @@ fp2Wrench = forceplates.upsampled.FP2.wrenches';
 
 % ---- FP1 --> transform FP1 data from FP1 frame to leftFoot frame
 % We want: leftFoot_f_fp1 starting from fp1_f_fp1
-leftSole_T_fp1Pos = iDynTree.Position();
-fp1SeenFromLeftSole = [0.099; 0.063 ; 0];
-leftSole_T_fp1Pos.fromMatlab(fp1SeenFromLeftSole);
-leftFoot_T_leftSolePos = iDynTree.Position();
-leftSoleSeenFromLeftFoot = [0.0; 0.0; ...
-                    subjectParamsFromData.leftFootBoxOrigin(3) - shoeHeight];
-leftFoot_T_leftSolePos.fromMatlab(leftSoleSeenFromLeftFoot);
-leftSole_R_fp1 = iDynTree.Rotation(); % ==leftFoot_R_fp1
+leftSole_R_fp1 = iDynTree.Rotation(); % ==leftFoot_R_fp1 ==leftHeel_R_fp1
 leftSole_R_fp1.fromMatlab ([ 0.0, -1.0,  0.0; ...
                             -1.0,  0.0,  0.0; ...
                              0.0,  0.0, -1.0]);
-leftFoot_T_fp1 = iDynTree.Transform(leftSole_R_fp1, ...
-             leftFoot_T_leftSolePos + leftSole_T_fp1Pos);
+leftSole_T_fp1Pos = iDynTree.Position();
+fp1SeenFromLeftSole = [0.099; 0.063 ; 0];
+leftSole_T_fp1Pos.fromMatlab(fp1SeenFromLeftSole);
 
-% transform the wrench in the proper frame and change the sign
+leftHeel_T_leftSolePos = iDynTree.Position();
+leftSoleSeenFromLeftHeel = [0.037; 0 ; -0.060];
+leftHeel_T_leftSolePos.fromMatlab(leftSoleSeenFromLeftHeel);
+
+leftFoot_T_leftHeelPos = iDynTree.Position();
+leftHeelSeenFromLeftFoot = subjectParamsFromData.pLeftHeelFoot;
+leftFoot_T_leftHeelPos.fromMatlab(leftHeelSeenFromLeftFoot);
+
+leftFoot_T_fp1 = iDynTree.Transform(leftSole_R_fp1, ...
+             leftFoot_T_leftHeelPos + leftHeel_T_leftSolePos + leftSole_T_fp1Pos);
+
+% Transform the wrench in the proper frame and change the sign
 forceplates.upsampled.FP1.humanLeftFootWrench = ...
               -1*(leftFoot_T_fp1.asAdjointTransformWrench().toMatlab()* ...
               fp1Wrench);
 
+% ------WRONG CODE
+% leftSole_T_fp1Pos = iDynTree.Position();
+% fp1SeenFromLeftSole = [0.099; 0.063 ; 0];
+% leftSole_T_fp1Pos.fromMatlab(fp1SeenFromLeftSole);
+% leftFoot_T_leftSolePos = iDynTree.Position();
+% leftSoleSeenFromLeftFoot = [0.0; 0.0; ...
+%                     subjectParamsFromData.leftFootBoxOrigin(3) - shoeHeight];
+% leftFoot_T_leftSolePos.fromMatlab(leftSoleSeenFromLeftFoot);
+% leftSole_R_fp1 = iDynTree.Rotation(); % ==leftFoot_R_fp1
+% leftSole_R_fp1.fromMatlab ([ 0.0, -1.0,  0.0; ...
+%                             -1.0,  0.0,  0.0; ...
+%                              0.0,  0.0, -1.0]);
+% leftFoot_T_fp1 = iDynTree.Transform(leftSole_R_fp1, ...
+%              leftFoot_T_leftSolePos + leftSole_T_fp1Pos);
+% ------WRONG CODE
+
+
 % ---- FP2 --> transform FP2 data from FP2 frame to rightFoot frame
 % We want: rightFoot_f_fp2 starting from fp2_f_fp2
-rightSole_T_fp2Pos = iDynTree.Position();
-fp2SeenFromRightSole = [0.099; -0.064 ; 0];
-rightSole_T_fp2Pos.fromMatlab(fp2SeenFromRightSole);
-rightFoot_T_rightSolePos = iDynTree.Position();
-rightSoleSeenFromRightFoot = [0.0; 0.0; ...
-                    subjectParamsFromData.rightFootBoxOrigin(3) - shoeHeight];
-rightFoot_T_rightSolePos.fromMatlab(rightSoleSeenFromRightFoot);
-rightSole_R_fp2 = iDynTree.Rotation(); % ==rightFoot_R_fp2
+rightSole_R_fp2 = iDynTree.Rotation(); % ==rightFoot_R_fp2 ==rightHeel_R_fp2
 rightSole_R_fp2.fromMatlab ([ 0.0, -1.0,  0.0; ...
                              -1.0,  0.0,  0.0; ...
                               0.0,  0.0, -1.0]);
+rightSole_T_fp2Pos = iDynTree.Position();
+fp2SeenFromRightSole = [0.099; -0.064 ; 0];
+rightSole_T_fp2Pos.fromMatlab(fp2SeenFromRightSole);
+
+rightHeel_T_rightSolePos = iDynTree.Position();
+rigthSoleSeenFromRightHeel = [0.037; 0 ; -0.060];
+rightHeel_T_rightSolePos.fromMatlab(rigthSoleSeenFromRightHeel);
+
+rightFoot_T_rightHeelPos = iDynTree.Position();
+rightHeelSeenFromRightFoot = subjectParamsFromData.pRightHeelFoot;
+rightFoot_T_rightHeelPos.fromMatlab(rightHeelSeenFromRightFoot);
+
 rightFoot_T_fp2 = iDynTree.Transform(rightSole_R_fp2, ...
-             rightFoot_T_rightSolePos + rightSole_T_fp2Pos);
+             rightFoot_T_rightHeelPos + rightHeel_T_rightSolePos + rightSole_T_fp2Pos);
 
 % transform the wrench in the proper frame and change the sign
 forceplates.upsampled.FP2.humanRightFootWrench = ...
               -1*(rightFoot_T_fp2.asAdjointTransformWrench().toMatlab()* ...
               fp2Wrench);
-
-%% Plot tmp
-
-% ----- FP1
-fig = figure();
-axes1 = axes('Parent',fig,'FontSize',16);
-              box(axes1,'on');
-              hold(axes1,'on');
-              grid on;
-len = length(forceplates.upsampled.FP2.humanRightFootWrench);
-              
-subplot (231) % comparison forces component x
-plot1 = plot(fp1Wrench(1,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(1,:),'r','lineWidth',1.5);
-ylabel('forces','HorizontalAlignment','center',...
-       'FontWeight','bold',...
-       'FontSize',18,...
-       'Interpreter','latex');
-xlim([0  len]);
-title ('x');
-grid on;
-
-subplot (232) % comparison forces component y 
-plot1 = plot(fp1Wrench(2,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(2,:),'r','lineWidth',1.5);
-title ('y');
-xlim([0  len]);
-grid on;
-
-subplot (233) % comparison forces component z
-plot1 = plot(fp1Wrench(3,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(3,:),'r','lineWidth',1.5);
-title ('z');
-xlim([0  len]);
-grid on;
-
-subplot (234) % comparison moment component x 
-plot1 = plot(fp1Wrench(4,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(4,:),'r','lineWidth',1.5);
-ylabel('moments','HorizontalAlignment','center',...
-       'FontWeight','bold',...
-       'FontSize',18,...
-       'Interpreter','latex');
-xlim([0  len]);
-grid on;
-
-subplot (235) % comparison moment component y
-plot1 = plot(fp1Wrench(5,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(5,:),'r','lineWidth',1.5);
-xlim([0  len]);
-grid on;
-
-subplot (236) % comparison moment component z 
-plot1 = plot(fp1Wrench(6,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP1.humanLeftFootWrench(6,:),'r','lineWidth',1.5);
-xlim([0  len]);
-grid on;
-
-leg = legend([plot1,plot2],{'FP1-frame','LeftFoot-frame'});
-set(leg,'Interpreter','latex', ...
-        'Position',[0.369020817175207 0.95613614004149 0.303215550427647 0.0305007585806261], ...
-       'Orientation','horizontal');
-set(leg,'FontSize',13);
-
-% ----- FP2
-fig = figure();
-axes1 = axes('Parent',fig,'FontSize',16);
-              box(axes1,'on');
-              hold(axes1,'on');
-              grid on;
-              
-subplot (231) % comparison forces component x
-plot1 = plot(fp2Wrench(1,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(1,:),'r','lineWidth',1.5);
-ylabel('forces','HorizontalAlignment','center',...
-       'FontWeight','bold',...
-       'FontSize',18,...
-       'Interpreter','latex');
-xlim([0  len]);
-title ('x');
-grid on;
-
-subplot (232) % comparison forces component y 
-plot1 = plot(fp2Wrench(2,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(2,:),'r','lineWidth',1.5);
-title ('y');
-xlim([0  len]);
-grid on;
-
-subplot (233) % comparison forces component z
-plot1 = plot(fp2Wrench(3,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(3,:),'r','lineWidth',1.5);
-title ('z');
-xlim([0  len]);
-grid on;
-
-subplot (234) % comparison moment component x 
-plot1 = plot(fp2Wrench(4,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(4,:),'r','lineWidth',1.5);
-ylabel('moments','HorizontalAlignment','center',...
-       'FontWeight','bold',...
-       'FontSize',18,...
-       'Interpreter','latex');
-xlim([0  len]);
-grid on;
-
-subplot (235) % comparison moment component y
-plot1 = plot(fp2Wrench(5,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(5,:),'r','lineWidth',1.5);
-xlim([0  len]);
-grid on;
-
-subplot (236) % comparison moment component z 
-plot1 = plot(fp2Wrench(6,:),'b','lineWidth',1.5);
-hold on 
-plot2 = plot(forceplates.upsampled.FP2.humanRightFootWrench(6,:),'r','lineWidth',1.5);
-xlim([0  len]);
-grid on;
-
-leg = legend([plot1,plot2],{'FP2-frame','RightFoot-frame'});
- set(leg,'Interpreter','latex', ...
-         'Position',[0.369020817175207 0.95613614004149 0.303215550427647 0.0305007585806261], ...
-        'Orientation','horizontal');
-set(leg,'FontSize',13);
 
 end
