2707b27a30dbb979d421289ee1e32698c28c3ce8
Luca Tagliapietra <luca.tagliapietra@iit.it>
2018-06-12 16:39:07 +0200

    Fix issues in batch kinematic data extraction

--- Experiments/23links_human/extractKinematicInfos.m
+++ Experiments/23links_human/extractKinematicInfos.m
@@ -17,34 +17,44 @@ end
 % 	fprintf('Sub folder #%d = %s\n', k, subFolders(k).name);
 % end
 
-group1 = true;
+group1 = false;
 if group1
     % GROUP 1
-    subjectID = [1,3,5,9,11];
+    subjectID = [1,3,5,7,9,11];
     taskID = [1];
 else
     % GROUP 2
-    subjectID = [2,4,6,8,10];
+    subjectID = [2,4,6,8,10,12];
     taskID = [0,2];
 end
 
 for subjIdx = 1 : length(subjectID)
+    pathToSubject = fullfile(bucket.datasetRoot, sprintf('S%02d',subjectID(subjIdx)));
+    
     if ~exist(fullfile(pathToKinFolder,sprintf('S%02d',subjectID(subjIdx))),'dir')
         mkdir(fullfile(pathToKinFolder,sprintf('S%02d',subjectID(subjIdx))));
     end
+    
     pathToSubjectFolder = fullfile(pathToKinFolder,sprintf('S%02d',subjectID(subjIdx)));
     for taskIdx = 1 : length(taskID)
+        pathToTask = fullfile(pathToSubject,sprintf('task%d',taskID(taskIdx)));
+        pathToProcessedData = fullfile(pathToTask,'processed');
+        
         if ~exist(fullfile(pathToSubjectFolder,sprintf('Task%d',taskID(taskIdx))),'dir')
             mkdir(fullfile(pathToSubjectFolder,sprintf('Task%d',taskID(taskIdx))));
         end
+        
         pathToTaskFolder = fullfile(pathToSubjectFolder,sprintf('Task%d',taskID(taskIdx)));
+        
+        % Copy URDF model
+        filenameURDF = fullfile(pathToSubject, sprintf('XSensURDF_subj%02d_48dof.urdf', subjectID(subjIdx)));
+        copyfile(filenameURDF,pathToSubjectFolder);
+        % Copy selectedJoints
+        copyfile(fullfile(pathToProcessedData,'selectedJoints.mat'),pathToTaskFolder);
+        % Copy groundBasePose
+        copyfile(fullfile(pathToProcessedData,'groundBasePose.mat'),pathToTaskFolder);
+        % Copy the kinematics
+        copyfile(fullfile(pathToProcessedData,'synchroKin.mat'),pathToTaskFolder);
     end
-    % Copy URDF model
-    copyfile(bucket.filenameURDF,pathToSubjectFolder);
-    % Copy selectedJoints
-    copyfile(fullfile(bucket.pathToProcessedData,'selectedJoints.mat'),pathToTaskFolder);
-    % Copy groundBasePose
-    copyfile(fullfile(bucket.pathToProcessedData,'groundBasePose.mat'),pathToTaskFolder);
-    % Copy the kinematics
-    copyfile(fullfile(bucket.pathToProcessedData,'synchroKin.mat'),pathToTaskFolder);
+
 end
