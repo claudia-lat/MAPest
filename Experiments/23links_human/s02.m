f2ba853d9fe8d370919b913f0a2d9d17e1ff25a2
Luca Tagliapietra <luca.tagliapietra@iit.it>
2018-06-13 14:09:25 +0200

    Add configuration scripts for all the subjects
    Required to batch process the data through multiple MATLAB instances

---
+++ Experiments/23links_human/s02.m
@@ -0,0 +1,43 @@
+clc; close all; clear all;
+
+rng(1); % Force the casual generator to be const
+format long;
+
+%% Add src to the path
+addpath(genpath('src'));
+addpath(genpath('templates'));
+addpath(genpath('../../src'));
+addpath(genpath('../../external'));
+
+%% Java path needed by OSIM - DO NOT UNCOMMENT-SCREWS MATLAB CONFIGURATION
+%  Routine left here just for Legacy, not to be used since it erases the
+%  current path. 
+%  setupJAVAPath();
+
+%% Configure
+% Root folder of the dataset
+bucket.datasetRoot = fullfile('D:\Datasets\2018_Feb_JSI');
+
+% Subjects and tasks to be processed
+subjectList = [2];
+taskList = [0 2];
+
+%% Run MAPest
+% Loop through all the subjects
+for s = 1:length(subjectList)
+    subjectID = subjectList(s);
+    
+    % Loop through all the tasks 
+    for t = 1:length(taskList)
+        taskID = taskList(t);
+        
+        % Clear the workspace apart for the variables required to run the
+        % loops
+        clearvars -except subjectList subjectID s taskList taskID t datasetRoot
+        bucket = struct;
+        bucket.datasetRoot = datasetRoot;
+        
+        % Run MAPest main
+        main;        
+    end % Tasks loop
+end % Subjects loop
