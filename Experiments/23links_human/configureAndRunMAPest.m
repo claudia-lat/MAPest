ccb17ebae9ae1da9a0fabb62ae693af660c5bfd1
Luca Tagliapietra <luca.tagliapietra@iit.it>
2018-06-13 14:04:27 +0200

    Clean-up configuration script

--- Experiments/23links_human/configureAndRunMAPest.m
+++ Experiments/23links_human/configureAndRunMAPest.m
@@ -1,7 +1,6 @@
+clc; close all; clear all;
 
-clc;close all;clear all;
-%clear;clc;close all;
-rng(1); % forcing the casual generator to be const
+rng(1); % Force the casual generator to be const
 format long;
 
 %% Add src to the path
@@ -10,18 +9,26 @@ addpath(genpath('templates'));
 addpath(genpath('../../src'));
 addpath(genpath('../../external'));
 
-%% Java path needed by OSIM
+%% Set Java path needed by OSIM - SCREWS MATLAB CURRENT CONFIGURATION
+%       UNCONMMENT ONLY IF YOU KNOW WHAT YOU ARE DOING
+%  Routine left here just for Legacy, not to be used since it erases the
+%  current path. 
+
 setupJAVAPath();
 
 %% Preliminaries
 % Create a structure 'bucket' where storing different stuff generating by
 % running the code
 bucket = struct;
-subjectID = 1;
-taskID = 1;
 
+%% Configure
+% Root folder of the dataset
 bucket.datasetRoot = fullfile(pwd, 'dataJSI');
 %bucket.datasetRoot = fullfile('D:\Datasets\2018_Feb_JSI');
 
-%% main.m calling
+% Subject and task to be processed
+subjectID = 1;
+taskID = 1;
+
+%% Run MAPest main.m
 main;
