54d572ad093ba8bb031a043d14a19ae5451139f2
Claudia Latella <claudia.latella.86@gmail.com>
2018-10-19 15:27:38 +0200

    Added function to extract estimated ext forces from mu_dgiveny

---
+++ Experiments/23links_human/src/extractEstimatedFext_from_mu_dgiveny.m
@@ -0,0 +1,15 @@
+function Fext = extractEstimatedFext_from_mu_dgiveny(berdy, dVectorOrder, mu_dgiveny)
+%EXTRACTEDESTIMATEDFEXT_FROM_MUDGIVENY extracts the estimated external
+% wrenches by MAP.
+
+nrOfLinks = size(dVectorOrder,1);
+range = zeros(nrOfLinks,1);
+nrOfSamples  = size(mu_dgiveny  ,2);
+
+Fext = zeros(6*size(dVectorOrder,1), nrOfSamples);
+for i = 1 : nrOfLinks
+    range(i,1) = (rangeOfDynamicVariable(berdy, iDynTree.NET_EXT_WRENCH, dVectorOrder{i}));
+    tmpRange = range(i,1) : range(i,1)+5;
+    Fext(6*(i-1)+1:6*i,:) = mu_dgiveny(tmpRange,:);
+end
+end
