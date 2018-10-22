ad560e4968733ffd7fbaf04a4cfa7bb9724ea5e1
Claudia Latella <claudia.latella.86@gmail.com>
2018-10-22 09:35:51 +0200

    Removed useless function

--- Experiments/23links_human/src/jointListForKinDynObject.m
+++
@@ -1,8 +0,0 @@
-function jointListForKinDynObject(kinDyn)
-
-    model = kinDyn.getRobotModel();
-    
-    for i = 0 : model.getNrOfJoints() -1
-        disp(model.getJointName(i));
-    end    
-end
