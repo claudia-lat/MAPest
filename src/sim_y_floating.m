d523167dc2125021c020ef99af1b176ef8b4a152
Claudia <claudia.latella.86@gmail.com>
2019-08-21 17:50:28 +0200

    Fixed code typo

--- src/sim_y_floating.m
+++ src/sim_y_floating.m
@@ -27,7 +27,7 @@ base_angVel = iDynTree.Vector3();
 for i = 1: length(human_state.q)
     
     q.fromMatlab(human_state.q(:,i));
-    dq.fromMatlab(human_state.q(:,i));
+    dq.fromMatlab(human_state.dq(:,i));
     base_angVel.fromMatlab(baseAngVel(:,i));
     
     berdy.updateKinematicsFromFloatingBase(q,dq,baseIndex,base_angVel);
